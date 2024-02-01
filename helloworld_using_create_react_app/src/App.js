62da4f5c87b8e75fb835ef46afd8ce4ff81993b6
NarendharReddy07 <gujjulanarendharreddy@gmail.com>
2024-02-01 12:52:47 +0530

    hello worls using create-react-app

---
+++ helloworld_using_create_react_app/src/App.js
@@ -0,0 +1,12 @@
+import HelloWorld from "./HelloWorld";
+function App() {
+  return (
+    <>
+        <HelloWorld />
+        <h1>hello world from App.js </h1>
+    </>
+  
+  );
+}
+
+export default App;
---
+++ helloworld_using_create_react_app/src/HelloWorld.js
@@ -0,0 +1,7 @@
+
+function HelloWorld(){
+    return(
+        <h1>hello world from HelloWorld Component</h1>
+    );
+}
+export default HelloWorld;
