8e3b9127e9da37fde3691bf6a9a9cc4a79cec2bb
NarendharReddy07 <gujjulanarendharreddy@gmail.com>
2024-02-01 13:22:28 +0530

    hello world using vite react

---
+++ HelloWorldUsingVite/src/App.jsx
@@ -0,0 +1,12 @@
+import HelloWorld from "./HelloWorld"
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+      <h1> hello world in App.jsx</h1>
+    </>
+  )
+}
+
+export default App
---
+++ HelloWorldUsingVite/src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HelloWorld() {
+  return (
+    <div>
+      <h4>hello world from helloworld.jsx</h4>
+    </div>
+  )
+}
